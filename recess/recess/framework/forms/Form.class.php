f6e75b66b24cbab120d22e08037fb6293f5b940d
Nicola Fontana <ntd@entidi.it>
2009-12-13 16:19:44 +0100

    <input> elements should be enclosed by a block container
    The <form> element cannot direclty contain <input> elements: enclosing
    the hidden input (the one forwarding the HTTP method) generated by the
    Form::begin() method in a <div class="hidden">.
    http://www.w3.org/TR/html401/interact/forms.html#edef-FORM

--- recess/recess/framework/forms/Form.class.php
+++ recess/recess/framework/forms/Form.class.php
@@ -10,18 +10,17 @@ Library::import('recess.framework.forms.HiddenInput');
 
 class Form {
 	protected $name;
-	
 	public $method;
 	public $action;
 	public $flash;
-	
+
 	function __construct($name) {
 		$this->name = $name;
 	}
-	
+
 	public $hasErrors = false;
 	public $inputs = array();
-	
+
 	function __get($name) {
 		if(isset($this->inputs[$name])) {
 			return $this->inputs[$name];
@@ -29,41 +28,41 @@ class Form {
 			return '';
 		}
 	}
-	
+
 //	function __set($name, $value) {
 //		if(isset($this->inputs[$name])) {
 //			$this->inputs[$name]->value = $value;
 //		}
 //	}
-	
+
 	function to($method, $action) {
 		$this->method = $method;
 		$this->action = $action;
 	}
-	
+
 	function begin() {
 		if($this->method == Methods::DELETE || $this->method == Methods::PUT) {
-			echo '<form method="post" action="' . $this->action . '">';
-			echo '<input type="hidden" name="_METHOD" value="' . $this->method . '" />';
+			echo '<form method="post" action="' . $this->action . '">', "\n";
+			echo '<div class="hidden"><input type="hidden" name="_METHOD" value="' . $this->method . '" /></div>';
 		} else {
 			echo '<form method="' . strtolower($this->method) . '" action="' . $this->action . '">';
 		}
 	}
-	
+
 	function input($name, $class = '') {
 		if($class != '') {
 			$this->inputs[$name]->class = $class;
 		}
 		$this->inputs[$name]->render();
 	}
-	
+
 	function changeInput($name, $newInput) {
 		$current = $this->inputs[$name];
 		$newInput .= 'Input';
 		$this->inputs[$name] = new $newInput($name);
 		$this->inputs[$name]->setValue($current->getValue());
 	}
-	
+
 	function fill(array $keyValues) {
 		foreach($this->inputs as $key => $value) {
 			if(isset($keyValues[$key])) {
@@ -71,7 +70,7 @@ class Form {
 			}
 		}
 	}
-	
+
 	function assertNotEmpty($inputName) {
 		if(isset($this->inputs[$inputName]) && $this->inputs[$inputName]->getValue() != '') {
 			return true;
@@ -82,11 +81,11 @@ class Form {
 			return false;
 		}
 	}
-	
+
 	function hasErrors() {
 		return $this->hasErrors;
 	}
-	
+
 	function end() {
 		echo '</form>';
 	}
